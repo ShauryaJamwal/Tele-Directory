c8f00a0eec405b106d6028b70fa6e14d7cc1a6db
ShauryaJamwal <shaurya.jamwal.9@gmail.com>
2020-04-10 18:45:51 +0530

    Updates

---
+++ precache-manifest.f0ac6a263c7124f0edbc7be523846d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02bdd58946d6341399deb5b022d5d9d",
+    "url": "/ShauryaJamwal.github.io/index.html"
+  },
+  {
+    "revision": "bd54cc8f7eddaf220beb",
+    "url": "/ShauryaJamwal.github.io/static/css/main.3f780d04.chunk.css"
+  },
+  {
+    "revision": "96c485de24d0f84b9522",
+    "url": "/ShauryaJamwal.github.io/static/js/2.b70f68aa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ShauryaJamwal.github.io/static/js/2.b70f68aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd54cc8f7eddaf220beb",
+    "url": "/ShauryaJamwal.github.io/static/js/main.8c568ec0.chunk.js"
+  },
+  {
+    "revision": "da5d8fc4d237324aa2fb",
+    "url": "/ShauryaJamwal.github.io/static/js/runtime-main.eaaaf111.js"
+  }
+]);
